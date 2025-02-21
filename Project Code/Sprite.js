cbc0e52658dae2b4d571b8cae669aa8b864a071f
iklafari <cromo@uccs.edu>
2025-02-20 22:47:06 -0700

    Christopher Romo Sprint 1 Week 1 Commit
    Added code following tutorials #1 and #2 in the RPG Guide using HTML, CSS, and JavaScript.

---
+++ Project Code/Sprite.js
@@ -0,0 +1,46 @@
+class Sprite {
+    constructor(config) {
+        // Set up the image
+        this.image = new Image();
+        this.image.src = config.src;
+        this.image.onload = () => {
+            this.isLoaded = true;   
+        }
+
+        // Shadow
+        this.shadow = new Image();
+        this.useShadow = true // config.useShadow || false
+        if (this.useShadow) {
+            this.shadow.src = "./images/characters/shadow.png";    
+        }
+        this.shadow.onload = () => {
+            this.isShadowLoaded = true;
+        }
+
+        // Configure Animation & Initial State
+        this.animations = config.animations || {
+            idleDown: [
+                [0,0]
+            ]
+        }
+        this.currentAnimation = config.currentAnimation || "idleDown";
+        this.currentAnimationFrame = 0;
+
+        // Reference the game object
+        this.gameObject = config.gameObject;
+    }
+
+    draw(ctx) {
+        const x = this.gameObject.x * 16 - 8;
+        const y = this.gameObject.y * 16 - 18;
+
+        this.isShadowLoaded && ctx.drawImage(this.shadow, x, y)
+        
+        this.isLoaded && ctx.drawImage(this.image,
+            0,0,    // left cut, top cut
+            32,32,  // width of cut, height of cut
+            x,y,    // position x, position y
+            32,32   // draw size x, draw size y
+        )
+    }
+}
---
+++ Project Code/init.js
@@ -0,0 +1,7 @@
+(function () {
+    // create an overworld object and initialize
+    const overworld = new Overworld({
+        element: document.querySelector(".game-container")
+    });
+    overworld.init();
+})();
