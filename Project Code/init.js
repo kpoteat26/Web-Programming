790be703ce0fa7520a717a0678150858f1aa452c
kpoteat26 <kpoteat@uccs.edu>
2025-03-01 23:07:59 -0700

    Kristen Poteat Sprint 1 Week 2 Completed Sprint (Added Collisions & Cutscene)

--- Project Code/init.js
+++ Project Code/init.js
@@ -1,8 +1,8 @@
 (function () {
-    // create an overworld object and initialize
-    const overworld = new Overworld({
-        element: document.querySelector(".game-container")
-    });
 
+    const overworld = new Overworld({
+      element: document.querySelector(".game-container")
+    });
     overworld.init();
-})();
+  
+  })();
